f19b372e2cc1ac9f025e17c8494c4a7f3983b2e2
adi1144 <adrianfiodorowicz@gmail.com>
2023-05-05 23:54:58 +0200

    AI imlepment
    add AIControllerClass
    add BehaviorTree
    add BlackBoard
    implemen base AIBehavior

--- Source/WizardRPG/Private/AI/SAICharacter.cpp
+++ Source/WizardRPG/Private/AI/SAICharacter.cpp
@@ -2,30 +2,28 @@
 
 
 #include "AI/SAICharacter.h"
+#include "SAttributionComponent.h"
 
 // Sets default values
 ASAICharacter::ASAICharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	AttriComp = CreateDefaultSubobject<USAttributionComponent>("AttriComp");
 
 }
 
-// Called when the game starts or when spawned
 void ASAICharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void ASAICharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
-
-// Called to bind functionality to input
+ 
 void ASAICharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
--- Source/WizardRPG/Public/AI/SAICharacter.h
+++ Source/WizardRPG/Public/AI/SAICharacter.h
@@ -5,25 +5,24 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "SAICharacter.generated.h"
-
+class USAttributionComponent;
 UCLASS()
 class WIZARDRPG_API ASAICharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ASAICharacter();
 
 protected:
-	// Called when the game starts or when spawned
+	UPROPERTY(EditAnywhere,Category="Mesh")
+		USAttributionComponent* AttriComp;
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
